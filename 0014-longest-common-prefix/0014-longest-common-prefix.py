6f2f0be0aab624b7f998d7679d4cf16bf5d40899
Moonding <moonding365@gmail.com>
2024-11-19 00:06:52 +0900

    Time: 15 ms (5.28%), Space: 16.9 MB (17.31%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        min_len = 200
+        min_len_str = ""
+        for str in strs:
+            str_len =len(str)
+            if str_len <= min_len:
+                min_len = str_len
+                min_len_str = str
+
+
+        for i,ch in enumerate(min_len_str):
+            for str in strs:
+                print(f"str[i]:{str[i]}")
+                print(f"ch:{ch}")
+                if str[i] !=ch:
+                    return min_len_str[:i]
+
+        return min_len_str
+
+
+        
+
