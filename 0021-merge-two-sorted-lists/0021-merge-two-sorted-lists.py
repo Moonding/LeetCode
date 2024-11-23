061b0b87a2d57f4ebd48eb2dfd6a55e535afdd01
Moonding <moonding365@gmail.com>
2024-11-24 01:13:17 +0900

    Time: 0 ms (100%), Space: 16.9 MB (10.51%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -9,10 +9,7 @@ class Solution:
         list3_head = None
         list3 = None
         
-        while list1 or list2:
-            #print(f"list1:{list1}")  
-            #print(f"list2:{list2}")      
-            #print(f"list3:{list3}\n")         
+        while list1 or list2:       
             if list1 is None or list2 and list1.val > list2.val:
                 if list3_head is None:
                     list3_head = ListNode(list2.val)
